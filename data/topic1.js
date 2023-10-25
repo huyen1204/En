e66557b1c0d0c0ae99be3ae0ba6cbd1dc26a6a12
huyen1204 <148972245+huyen1204@users.noreply.github.com>
2023-10-25 22:45:40 +0900

    Update topic1.js

--- data/topic1.js
+++ data/topic1.js
@@ -1,18 +1,72 @@
 const data = [
     {
-        "word": "confirm",
-        "type": "verb",
-        "definition": "to prove that a belief or an opinion that was previously not completely certain is true",
-        "example": "",
-        "pronunciation": "",
-        "picture": ""
+        "word": "stab someone in the back",
+        "type": "",
+        "definition": "to do something harmful to someone who trusted you"
     },
     {
-        "word": "demonstrate",
+        "word": "betray",
         "type": "verb",
-        "definition": "to show or make something clear",
-        "example": "",
-        "pronunciation": "",
-        "picture": ""
+        "definition": "If someone betrays something such as a promise, they do not do what they promised"
+    },
+    {
+        "word": "kill two birds with one stone",
+        "type": "",
+        "definition": "to succeed in achieving two things in a single action"
+    },
+    {
+        "word": "fall apart",
+        "type": "",
+        "definition": "to break into pieces"
+    },
+    {
+        "word": "spill the beans",
+        "type": "",
+        "definition": "to tell people secret information"
+    },
+    {
+        "word": "stick to your guns",
+        "type": "",
+        "definition": "to continue to have your beliefs or continue with a plan of action, even if other people disagree with you"
+    },
+    {
+        "word": "have bigger/other fish to fry",
+        "type": "",
+        "definition": "to have something more important to do"
+    },
+    {
+        "word": "a clean slate",
+        "type": "",
+        "definition": "a state in which you are starting an activity or process again, not considering what has happened in the past at all"
+    },
+    {
+        "word": "have a bone to pick with someone",
+        "type": "",
+        "definition": "to want to talk to someone about something annoying they have done"
+    },
+    {
+        "word": "set eyes on someone/something",
+        "type": "",
+        "definition": "to watch someone/something carefully"
+    },
+    {
+        "word": "call someone's bluff",
+        "type": "",
+        "definition": "to make someone prove that what they are saying is true, or to make someone prove that they will really do what they say, because you do not believe them"
+    },
+    {
+        "word": "not have a leg to stand on",
+        "type": "",
+        "definition": "to have no support"
+    },
+    {
+        "word": "stab someone in the back",
+        "type": "",
+        "definition": "to do something harmful to someone who trusted you, A betrayal of trust, an act of treachery."
+    },
+    {
+        "word": "confession is good for the soul",
+        "type": "",
+        "definition": "Proverb( If you have done something wrong, you will feel better if you confess that you did it)"
     }
 ]
