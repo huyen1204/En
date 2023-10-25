ae54965742ae338f8a9c88e1c8dfb91836841ca5
Nguyen Huyen <huyennguyen.uet@vnu.edu.vn>
2023-10-25 22:03:19 +0900

    initial commit

---
+++ data/topic1.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        "word": "confirm",
+        "type": "verb",
+        "definition": "to prove that a belief or an opinion that was previously not completely certain is true",
+        "example": "",
+        "pronunciation": "",
+        "picture": ""
+    },
+    {
+        "word": "demonstrate",
+        "type": "verb",
+        "definition": "to show or make something clear",
+        "example": "",
+        "pronunciation": "",
+        "picture": ""
+    }
+]
